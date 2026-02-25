fa67f96c6697b0ec31d92e4ccdd1a2d106a13754
Lewis Cramb <lewiscramb@icloud.com>
2026-02-25 11:47:45 +0000

    project js and css done

--- JS/script.js
+++ JS/script.js
@@ -23,5 +23,73 @@ function navClear(){ //this hides the nav bar and brings back the main page
 
 
 
+//project cycling stuff
+
+class project{ //probs not conventionally great but it works for the means of this project
+    constructor(title, link, img, txt){
+        this.title = title ;
+        this.link = link ;
+        this.img = img ;
+        this.txt = txt ;
+    }
+
+    getTitle(){
+        return this.title ;
+    }
+
+    getLink() {
+        return this.link ;
+    }
+
+    getImg() {
+        return this.img ;
+    }
+
+    getTxt() {
+        return this.txt ;
+    }
+}
+
+const proj_track = new project("Project Tracker", "project_tracker", "pj-tkr.png", "This is the first project in my portfolio, it is a java program to track the due dates of exams or any assessments. It can store the data inputted and it will reformat and display the data on every opening of the project.")
+const port_web = new project("Portfolio Website", "portfolio_site", "site.png", "This is the website you are on now, I had to re-teach myself HTML and CSS to do this as well as learning JS on the fly using tutorials when I needed interactivity such as the projects list you are currently going through. It will be updated as my github updates, manually, and will be a larger version of my CV, basically.")
+const drill_mod = new project("Minecraft mod - Drill", "N/A", "N/A", "Will be updated, had idea for minecraft mod for a drill that mines 3x3 instantly in looking direction and dont want to forget.")
+
+const projects = [proj_track, port_web, drill_mod] ;
+let projIndex = 0 ; 
+
+function moveProjectRight(){
+    projIndex ++ ;
+    if (projIndex == projects.length){
+        projIndex = 0 ;
+    }
+
+    rewriteProjBox()
+}
+
+function moveProjectLeft(){
+    projIndex -- ;
+    if (projIndex == -1){
+        projIndex = projects.length-1 ;
+    }
+
+    rewriteProjBox() 
+}
+
+function rewriteProjBox(){
+    document.getElementById("proj-name").innerHTML = projects[projIndex].getTitle() ;
+
+    document.getElementById("proj-img").src = '../Images/' + projects[projIndex].getImg() ;
+
+    let url = projects[projIndex].getLink() ;
+    if (url == "N/A"){
+        document.getElementById("proj-url").href = "javascript:void(0)" ;
+    } else {
+        document.getElementById("proj-url").href = 'https://www.github.com/lewis-cramb/' + url ;
+    }
+
+    document.getElementById("proj-text").innerHTML = projects[projIndex].getTxt() ;
+}
+
+
 
 
--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 I will be using HTML, CSS and JS in order to create a portfolio website for all of my projects, education and experience
 
 # notes
-- I am not sure how to actually host this site, will figure it out (preferrbly not paying but will see prices) - (Between commit 5 and 6) Just done CO2026 from GUTS, try using github to host the site 
+- I am not sure how to actually host this site, will figure it out (preferrbly not paying but will see prices) - (Between commit 5 and 6) Just done CO2026 from GUTS, try using github to host the site - (Between commit 7 and 8) its just stopped working, dont use pages
 - I also have never touched javascript besides onMouseClick() so I will need to learn JS as I go along with this project.
+- Between commit 7 and 8, I want to try and update the total css of the page somehow to feel better than a simple N5 website, not sure how I will do that but I think something like bootstrap may be what I am after.
 
 
 # commit history
@@ -20,9 +21,11 @@ e33ed36 - Fifth commit, I edited the wireframe slightly to fit the page better a
 
 7d3db63 - Sixth commit, added the css and html such that when the nav button is pressed the screen dimmed and reset when clicked off of the nav bar. I also learned about and used GitHub Pages to run my website.
 
-[commit num] - Seventh commit (Technically nineth), messed around with Github Pages so commit history is slightly messy (this is the defitintive history). Fixed nav bar issues, moved index.html to fit with gh pages better, moved all css back to style.css and implemented the projects.html page.
+7de245a - Seventh commit (Technically nineth), messed around with Github Pages so commit history is slightly messy (this is the defitintive history). Fixed nav bar issues, moved index.html to fit with gh pages better, moved all css back to style.css and implemented the projects.html page.
+
+[commit num] - Eighth commit, added the js function for the projects page so that the details of a project can switch on a button click. Added the css for the page too so that it is styled the same as the wireframe and the rest of the site.
 
 # Links
 
 Javascript - https://www.w3schools.com/js/
-GitHub site hosting - https://www.geeksforgeeks.org/git/how-to-host-a-website-on-github-for-free/
+Triangle buttons on projects page = https://www.geeksforgeeks.org/css/how-to-create-a-triangle-using-css-clip-path/
